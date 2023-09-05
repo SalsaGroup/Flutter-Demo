09b28cdc16325194363cf62ea5683837ef0ae2bc
Huell-Howitzer <47521696+Huell-Howitzer@users.noreply.github.com>
2023-09-05 00:10:03 -0700

    Update firebaseConfig.js

--- web/firebaseConfig.js
+++ web/firebaseConfig.js
@@ -4,12 +4,12 @@
 // web/firebase-config.js
 
 let firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+    apiKey: envVariables.apiKey,
+    authDomain: envVariables.authDomain,
+    projectId: envVariables.projectID,
+    storageBucket: envVariables.storageBucket
+    messagingSenderId: envVariables.messagingSenderID,
+    appId: envVariables.appID
 };
 
 // Initialize Firebase
