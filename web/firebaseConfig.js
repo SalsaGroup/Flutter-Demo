c87eb41235b0df9a540823d3f2fde2d83ebb3d27
Huell-Howitzer <47521696+Huell-Howitzer@users.noreply.github.com>
2023-09-05 00:21:57 -0700

    Update firebaseConfig.js

--- web/firebaseConfig.js
+++ web/firebaseConfig.js
@@ -4,14 +4,13 @@
 // web/firebase-config.js
 
 let firebaseConfig = {
-    apiKey: envVariables.apiKey,
-    authDomain: envVariables.authDomain,
-    projectId: envVariables.projectID,
-    storageBucket: envVariables.storageBucket,
-    messagingSenderId: envVariables.messagingSenderID,
-    appId: envVariables.appID
+    apiKey: "AIzaSyDusZC28LsEBVgso3fHcYlupGLLfUO3mak",
+    authDomain: "kitchenai-cf3fb.firebaseapp.com",
+    projectId: "kitchenai-cf3fb",
+    storageBucket: "kitchenai-cf3fb.appspot.com",
+    messagingSenderId: "497667047418",
+    appId: "1:497667047418:web:4666c7d07f86ddc430ee43"
 };
-
 // Initialize Firebase
 console.log('FIREBASE_API_KEY:', process.env.FIREBASE_API_KEY);
 firebase.initializeApp(firebaseConfig);
