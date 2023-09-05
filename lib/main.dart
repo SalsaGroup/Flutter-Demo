b8ad400cbb4505d50856e13241ed23c2c806b1a0
Ryan M Howell <rmhowell@protonmail.com>
2023-09-04 23:11:57 -0700

    WIP

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,13 @@ Future<void> main() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
       options: FirebaseOptions(
-          apiKey: dotenv.get("FIREBASE_API_KEY"),
-          authDomain: dotenv.get("FIREBASE_AUTH_DOMAIN"),
-          projectId: dotenv.get("FIREBASE_PROJECT_ID"),
-          storageBucket: dotenv.get("FIREBASE_STORAGE_BUCKET"),
-          messagingSenderId: dotenv.get("FIREBASE_MESSAGING_SENDER_ID"),
-          appId: dotenv.get("FIREBASE_APP_ID"),
-          measurementId: dotenv.get("FIREBASE_MEASUREMENT_ID")),
+          apiKey: dotenv.get("apiKey"),
+          authDomain: dotenv.get("authDomain"),
+          projectId: dotenv.get("projectID"),
+          storageBucket: dotenv.get("storageBucket"),
+          messagingSenderId: dotenv.get("messagingSenderID"),
+          appId: dotenv.get("appId"),
+          measurementId: dotenv.get("measurementID")),
     );
   } else {
     await Firebase.initializeApp();
